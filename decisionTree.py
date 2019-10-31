a8ee97e7d40b99a726bcec278bb3f94452ae6438
sarkunz <sarahmykelle@gmail.com>
2019-10-31 08:33:49 -0600

    Graphs and extra dataset

--- decisionTree.py
+++ decisionTree.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
+import math
 
 ### NOTE: The only methods you are required to have are:
 #   * predict
@@ -7,7 +8,7 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 #   * score
 
 class Node():
-    def __init__(self, level, val="", col="", childs=[]):
+    def __init__(self, level, val=None, col=None, childs=[]):
         self.value = val
         self.feature = col
         self.children = childs
@@ -27,9 +28,7 @@ class DTClassifier(BaseEstimator,ClassifierMixin):
         self.featureCounts = count
         self.featureLabels = labels
         self.shuffle = shuffle
-        self.head = Node(0)
-
-        self.g = 0
+        self.head = Node(0, -1)
 
 
     """ Fit the data; Make the Desicion tree
@@ -41,67 +40,88 @@ class DTClassifier(BaseEstimator,ClassifierMixin):
     """
     def fit(self, X, y):
         self.splitData(X, y, self.head, self.featureLabels.copy())
+        print("*********PRINTING NODES***********")
+        self.printTree(self.head)
 
         return self
 
     def getGains(self, array, labels):
-        arySize = np.size(labels)
         allData = self.concatLabels(array.reshape(-1,1), labels.reshape(-1,1))
+        arySize = np.size(labels)
         vals = np.unique(allData[:,0])
-        print("allData", allData)
-        print("vals", vals)
-        splits = []
-        for i in range(np.size(vals)):
-            splits.append(allData[allData[:,0] == vals[i]])
-            #print("index", index)
-            # indexes.append(index)
-            # props.append(np.size(index)/arySize)
-        # print("props", props)
-        print("splits", splits)
-        outps = np.unique(labels)
+        features = []
         sum = 0
-        for outp in outps:
-            ha = 1
-        
-        return 1
+        #split up by value (Y/N etc)
+        for i in range(np.size(vals)):
+            features.append(allData[allData[:,0] == vals[i]])
+        #split each of those by output
+        for feature in features: #i in range(np.size(features)):
+            outps = np.unique(feature[:,1])
+            p = np.size(feature,0)/arySize #outerCoef
+            #sum output classes
+            for outp in outps:
+                hasOutp = np.where(feature[:,1] == outp)
+                innerCoef = np.size(hasOutp)/np.size(feature, 0)
+                featSum = p * (-innerCoef * math.log(innerCoef, 2))
+                sum += featSum
+                
+        return sum
 
     def concatLabels(self, X, y):
         allData = np.concatenate((X, y), axis=1)
         return allData
 
+    def printTree(self, node):
+        if(node.level < 5):
+            print("feature", node.feature, "val", node.value, "level", node.level)
+        if(node.children == None): return
+        for child in node.children:   
+            self.printTree(child)
+
     #init call with node=head
     def splitData(self, X, y, node, labelsList):
-        print("-------X size", np.size(X,1))
-        print("Num Entries", np.size(y, 0))
-        print("labelsList", labelsList)
-        print("node.val", node.value, "nodeLevel", node.level)
+        # print("-------X size", np.size(X,1))
+        # print("Num Entries", np.size(y, 0))
+        # print("labelsList", labelsList)
+        # print("node.val", node.value, "nodeLevel", node.level)
+
         concatData = self.concatLabels(X, y)
-        gains = []
         if(np.size(X, 1) == 1): 
+            #final split and append targets (as feature)
+            vals = np.unique(X)
+            node.feature = labelsList[0]
+            for val in vals:
+                ind = np.where(X.flatten() == val)[0][0]
+                target = y[int(ind),0]
+                child = Node(node.level + 1, val, target, None) #level, val=None, feature=None, childs=[]):
+                node.children.append(child)
             return
-        #calculate which node to split on (max gain)
-        gains.clear()
-        
+        gains = []
+        #calculate which node to split on (min entropy)
         for i in range(np.size(X,1)):
             gains.append(self.getGains(X[:,i], y))
-        maxGainz = max(gains)
+        minGainz = min(gains) #it says gains but it's acually entropy
         
         #split the data and build the tree
-        colInd = gains.index(maxGainz)
-        print("colInd", colInd)
+        colInd = gains.index(minGainz)
         #find colIndex in actual dataset
         colName = labelsList[colInd]
-        print("col to delete", colName)
         node.feature = colName #do label of column
-        childVals = np.unique(X[:,colInd])
-        labelsList.remove(colName)
+        childVals = self.featureCounts[self.featureLabels.index(colName)] 
+        cutLabels = labelsList.copy()
+        cutLabels.remove(colName)
 
-        for val in childVals:
+        for val in range(childVals):
             sData = concatData[concatData[:,colInd] == val] #all vals in splitData with that val
             sData = np.delete(sData, colInd, 1) #then del col
-            childNode = Node(node.level + 1, val, None, [])
-            node.children.append(childNode)
-            self.splitData(sData[:,:sData.shape[1]-1], sData[:,sData.shape[1]-1:], childNode, labelsList)
+            if(np.size(sData) == 0):#if there is no data for this va
+                target = np.average(y)
+                child = Node(node.level + 1, val, target, None) #level, val=None, feature=None, childs=[]):
+                node.children.append(child)
+            else: 
+                childNode = Node(node.level + 1, val, None, [])
+                node.children.append(childNode)
+                self.splitData(sData[:,:sData.shape[1]-1], sData[:,sData.shape[1]-1:], childNode, cutLabels)
         return
 
 
@@ -113,17 +133,39 @@ class DTClassifier(BaseEstimator,ClassifierMixin):
             Predicted target values per element in X.
     """
     def predict(self, X):
+        node = self.head
+        pred = ""
+        lastInd = ""
+        while(node.children != None):
+            colInd = self.featureLabels.index(node.feature)
+            for child in node.children:
+                if(lastInd == colInd):
+                    return child.feature
+                if X[colInd] == child.value:
+                    pred = child.feature
+                    node = child
+                    break
+            lastInd = colInd
+        return pred
 
-        pass
-
-
+    """ Return accuracy of model on a given dataset. Must implement own score function.
+    Args:
+        X (array-like): A 2D numpy array with data, excluding targets
+        y (array-li    def _shuffle_data(self, X, y):
+    """
     def score(self, X, y):
-        """ Return accuracy of model on a given dataset. Must implement own score function.
-        Args:
-            X (array-like): A 2D numpy array with data, excluding targets
-            y (array-li    def _shuffle_data(self, X, y):
-        """
-        return 0
+        correctCount = 0
+        totalCount = 0
+
+        for inputs, exp in zip(X, y):
+            pred = self.predict(inputs)
+            if(exp == pred): correctCount += 1
+            totalCount += 1
+
+        self.accuracy = correctCount/totalCount
+        #self.accArray.append(self.accuracy)
+        #find num outputs that we got right
+        return self.accuracy
 
     def _shuffle_data(self, X, y):
         """ Shuffle the data! This _ prefix suggests that this method should only be called internally.
--- main.py
+++ main.py
@@ -3,28 +3,96 @@ from arff import Arff
 import numpy as np
 import pandas as pd
 import graph_tools
-import string; 
+import string
+import math
+
+import pydotplus
+from sklearn import tree
+import collections
+from graphviz import Source
+from IPython.display import Image
 
 
-mat = Arff("datasets/lenses.arff")
+mat = Arff("datasets/tictactoe.arff")
 
 counts = [] ## this is so you know how many types for each column
-
 for i in range(mat.data.shape[1]):
        counts += [mat.unique_value_count(i)]
 data = mat.data[:,0:-1]
 labels = mat.data[:,-1].reshape(-1,1)
 
-labelNames = [char for char in string.ascii_lowercase[:data.shape[1]]]
+labelNames =  mat.get_attr_names() #[char for char in string.ascii_lowercase[:data.shape[1]]]
+#del labelNames[-1]
 
-DTClass = DTClassifier(counts, labelNames)
-DTClass.fit(data,labels)
+DTSClass = DTClassifier(counts, labelNames, shuffle=True)
+
+##########10 fold CV#########################
+# # #split into 10 groups
+# sData = np.array_split(data, 10, 0)
+# sLabels = np.array_split(labels, 10, 0)
+# accs = []
+
+# # clf = tree.DecisionTreeClassifier(min_impurity_decrease=2)
+
+# for i in range(1):
+#        #print(i, inputs[i])
+#        vData = np.copy(sData[i])
+#        vLabels = np.copy(sLabels[i]) 
+
+#        cData = list.copy(sData)
+#        cLabels = list.copy(sLabels)
+#        del cData[i]
+#        del cLabels[i]
+
+#        tData = np.concatenate(np.copy(cData[0:8], 0))
+#        tLabels = np.concatenate(np.copy(cLabels[0:8], 0))
+
+#        #Sklearn
+#        # model = clf.fit(tData, tLabels)
+#        # acc = clf.score(vData, vLabels)
+
+#        #My DT
+#        DTSClass.fit(tData, tLabels)
+#        acc = DTSClass.score(vData, vLabels)
+
+#        accs.append(acc)
+# print("ACCURACIES")
+# print(accs)
+# print("AVG ACC", sum(accs)/len(accs))
 
 
-# mat2 = Arff("/datasets/all_lenses.arff")
+
+###########Just run it normal#####################
+# DTClass = DTClassifier(counts, labelNames)
+# DTClass.fit(data,labels)
+# acc = DTClass.score(data, labels)
+# print("--ACCURACY 1--", acc)
+
+# mat2 = Arff("datasets/all_lenses.arff")
 # data2 = mat2.data[:,0:-1]
 # labels2 = mat2.data[:,-1]
-# pred = DTClass.predict(data2)
+
+# preds = []
+# for inputs in data2:
+#        pred = DTClass.predict(inputs)
+#        preds.append(pred)
+# np.savetxt("idk.csv",preds,delimiter=",")
+
 # Acc = DTClass.score(data2,labels2)
-# np.savetxt("pred_lenses.csv",pred,delimiter=",")
 # print("Accuracy = [{:.2f}]".format(Acc))
+
+################Sklearn stuff######################
+clf = tree.DecisionTreeClassifier(max_depth=5, min_samples_split=5)
+model = clf.fit(data, labels)
+print("SKLEARN ACC:", clf.score(data, labels))
+
+
+#Graph stuff
+featureNames = mat.get_attr_names()
+del featureNames[-1]
+graph = Source( tree.export_graphviz(model, feature_names=featureNames))
+png_bytes = graph.pipe(format='png')
+with open('tictac_tree.png','wb') as f:
+    f.write(png_bytes)
+
+Image(png_bytes)
